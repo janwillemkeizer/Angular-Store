ccab218b2e5503a448c36cadfc42b005293748b8
Jan Willem Keizer <jan.willem.keizer@newskool.nl>
2021-11-09 15:15:29 +0100

    Fix Definite Assignment Assertion

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -7,6 +7,8 @@ import { Product } from '../products';
   styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent implements OnInit {
+  @Input()
+  product!: Product;
 
   constructor() { }
 
